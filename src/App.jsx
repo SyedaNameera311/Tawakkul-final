dfc1c92b783868896fd6b4e447803ce5c34667d9
syeda-namira <syedanamira311@gmail.com>
2026-08-24 21:24:17 +0500

    Fix React route components

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import "./App.css";
 import Start from "./Components/start";
 import Navbar from "./Components/navbar";
 import Course from "./Components/course";
-import CourseDetail from "./Components/CourseDetail";
+import CourseDetail from "./Components/courseDetail";
 import Home from "./Components/home";
 import Modesty from "./Components/modesty";
 import ModestyDetails from "./Components/modestyDetails";
@@ -40,7 +40,7 @@ function AppRoutes() {
         <Route path="/course/:courseId" element={<CourseDetail />} />
 
         <Route path="/modesty" element={<Modesty />} />
-        <Route path="/modesty-details/:id" element={<ModestyDetails />} />
+   <Route path="/modesty-details/:id" element={<ModestyDetails />} />
 
         <Route path="/media" element={<Media />} />
         <Route path="/build" element={<Build />} />
